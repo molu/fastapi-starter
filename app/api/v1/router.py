a589124bbe7c17980de58d036e6dd757678d72c8
molu <mmol@tutanota.com>
2022-09-15 10:26:52 +0200

    Minor reformatting

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints.health_check import router as health_check_router
 
+from app.api.v1.endpoints.health_check import router as health_check_router
 
 api_router = APIRouter()
 api_router.include_router(health_check_router, tags=["health_check"])
--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -4,6 +4,7 @@ from httpx import AsyncClient
 from app.core.config import settings
 from app.main import app
 
+
 @pytest.fixture(scope="session")
 async def client() -> AsyncClient:
     async with AsyncClient(app=app, base_url=settings.BASE_URL) as client:
